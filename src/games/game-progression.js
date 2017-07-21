4fc10fe33ebd4c77f09c13f20a07567f1cd413ec
tysky <dtysky51@gmail.com>
2017-07-21 22:02:04 +0300

    add logic to progression game

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -6,7 +6,7 @@ const message = 'What number is missing in this progression?';
 const progressionGame = () => {
   const diff = Math.floor(Math.random() * 10);
   const firstNumber = Math.floor(Math.random() * 10);
-  // const place = Math.floor(Math.random() * 8) + 2;
+  const place = Math.floor(Math.random() * 8) + 2;
   // console.log(place);
   const progression = (startNumber, step) => {
     const iter = (prev, n) => {
@@ -15,10 +15,10 @@ const progressionGame = () => {
       }
       return `${prev + step} ${iter(prev + step, n - 1)}`;
     };
-    return `${startNumber} ${iter(startNumber, 4)} .. ${iter((startNumber + (5 * step)), 4)}`;
+    return `${iter(startNumber, place - 1)} .. ${iter((startNumber + (place * step)), 10 - place)}`;
   };
   const question = progression(firstNumber, diff);
-  const correctAnswer = firstNumber + (5 * diff);
+  const correctAnswer = String(firstNumber + (place * diff));
   return cons(question, correctAnswer);
 };
 
