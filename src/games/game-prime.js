1097577c2df043579ec2b912d937539d47789b46
tysky <dtysky51@gmail.com>
2017-07-22 22:36:49 +0300

    update game-prime logic

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -8,13 +8,13 @@ const primeGame = () => {
   const isPrime = (number) => {
     for (let i = 2; i <= Math.sqrt(number); i += 1) {
       if (number % i === 0) {
-        return 'no';
+        return false;
       }
     }
-    return 'yes';
+    return true;
   };
   const question = randomNumber;
-  const correctAnswer = isPrime(randomNumber);
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
   return cons(question, correctAnswer);
 };
 
