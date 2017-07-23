13781771f40e17f9b16f2d547cd600bcad87745c
tysky <dtysky51@gmail.com>
2017-07-23 15:53:48 +0300

    isolate function of getting progression member

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,27 +1,25 @@
 import { cons } from 'hexlet-pairs';
 import { playGame } from '..';
+import getRandomNumber from '../generate-random-number';
 
 const message = 'What number is missing in this progression?';
 
-const getMember = (a1, d, n, index) => {
-  if (n === index) {
-    return '..';
-  }
-  return (a1 + (d * (n - 1)));
-};
+const getMemberOfProgression = (a1, d, n) => a1 + (d * (n - 1));
+
 const generateProgression = (start, step, index) => {
-  const iter = (member) => {
-    if (member === 10) {
-      return getMember(start, step, member, index);
+  const iter = (n) => {
+    const current = n === index ? '..' : getMemberOfProgression(start, step, n);
+    if (n === 10) {
+      return current;
     }
-    return `${getMember(start, step, member, index)} ${iter(member + 1)}`;
+    return `${current} ${iter(n + 1)}`;
   };
   return iter(1);
 };
 const progressionGame = () => {
-  const step = Math.floor(Math.random() * 10);
-  const startNumber = Math.floor(Math.random() * 10);
-  const index = Math.floor(Math.random() * 10) + 1;
+  const step = getRandomNumber(0, 10);
+  const startNumber = getRandomNumber(0, 10);
+  const index = getRandomNumber(1, 10);
   const question = generateProgression(startNumber, step, index);
   const correctAnswer = String(startNumber + ((index - 1) * step));
   return cons(question, correctAnswer);
